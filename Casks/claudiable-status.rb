b71687ea23c651ead07e38cd317a32c04c158a25
Đông Phạm <pdong15dth06@gmail.com>
2026-03-01 00:58:49 +0700

    Update claudiable-status to 1.5.0

--- Casks/claudiable-status.rb
+++ Casks/claudiable-status.rb
@@ -1,8 +1,8 @@
 cask "claudiable-status" do
-  version "1.4.0"
-  sha256 "9613de2af2e7702dfe98aa29d271405ced3b1fc1566496dab2df00881a6382a8"
+  version "1.5.0"
+  sha256 "90e90e546be63ede961a71a0d55484d4aaf3e4ebce4f04ec176e4c530573f420"
 
-  url "https://github.com/pdong15dth/claudiable_status/releases/download/v1.4.0/claudiable_status.zip"
+  url "https://github.com/pdong15dth/claudiable_status/releases/download/v1.5.0/claudiable_status.zip"
   name "Claudible Status"
   desc "Menu bar dashboard for Claudible status"
   homepage "https://github.com/pdong15dth/claudiable_status"
