4cc771986acbff3373735b27a579ddacad6297ff
Đông Phạm <pdong15dth06@gmail.com>
2026-02-28 22:41:11 +0700

    Update app name to Claudiable Status

--- Casks/claudiable-status.rb
+++ Casks/claudiable-status.rb
@@ -3,9 +3,9 @@ cask "claudiable-status" do
   sha256 "1a517b4220104270f31fe6c52bc355def1128a7dcc56a178807c58f333b30fe6"
 
   url "https://github.com/pdong15dth/claudiable_status/releases/download/v#{version}/claudiable_status.zip"
-  name "claudiable_status"
-  desc "Menu bar dashboard for Claudible status"
+  name "Claudiable Status"
+  desc "Menu bar dashboard for Claudiable status"
   homepage "https://github.com/pdong15dth/claudiable_status"
 
-  app "claudiable_status.app"
+  app "Claudiable Status.app"
 end
