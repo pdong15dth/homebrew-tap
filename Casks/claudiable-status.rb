7ae3a67a33b6cc54823ffa66d361367e5dc1fb98
Đông Phạm <pdong15dth06@gmail.com>
2026-02-28 23:29:13 +0700

    Rename display name to Claudible Status

--- Casks/claudiable-status.rb
+++ Casks/claudiable-status.rb
@@ -3,9 +3,9 @@ cask "claudiable-status" do
   sha256 "2f44024871e01d84f239d96fea6d4807bc10e94bb680ffac87458ddaa8767ed0"
 
   url "https://github.com/pdong15dth/claudiable_status/releases/download/v#{version}/claudiable_status.zip"
-  name "Claudiable Status"
-  desc "Menu bar dashboard for Claudiable status"
+  name "Claudible Status"
+  desc "Menu bar dashboard for Claudible status"
   homepage "https://github.com/pdong15dth/claudiable_status"
 
-  app "Claudiable Status.app"
+  app "Claudible Status.app"
 end
