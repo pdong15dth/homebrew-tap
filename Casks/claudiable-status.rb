960d0859d87c1ec793a48284082c06cd36095844
Đông Phạm <pdong15dth06@gmail.com>
2026-02-28 23:15:03 +0700

    Update claudiable-status to 1.1.0 with correct SHA256

--- Casks/claudiable-status.rb
+++ Casks/claudiable-status.rb
@@ -1,6 +1,6 @@
 cask "claudiable-status" do
   version "1.1.0"
-  sha256 "5ea7355127503c3b6e3d6bcbd4baf64e886d74a971919b2296f39e43ba734da6"
+  sha256 "2f44024871e01d84f239d96fea6d4807bc10e94bb680ffac87458ddaa8767ed0"
 
   url "https://github.com/pdong15dth/claudiable_status/releases/download/v#{version}/claudiable_status.zip"
   name "Claudiable Status"
