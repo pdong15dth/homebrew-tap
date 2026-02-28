b27adc3aae3e91c708d788c1f3175b4dba085a29
Đông Phạm <pdong15dth06@gmail.com>
2026-02-28 22:41:42 +0700

    Update to v1.1.0 with Claudiable Status app name

--- Casks/claudiable-status.rb
+++ Casks/claudiable-status.rb
@@ -1,6 +1,6 @@
 cask "claudiable-status" do
-  version "1.0.2"
-  sha256 "1a517b4220104270f31fe6c52bc355def1128a7dcc56a178807c58f333b30fe6"
+  version "1.1.0"
+  sha256 "5ea7355127503c3b6e3d6bcbd4baf64e886d74a971919b2296f39e43ba734da6"
 
   url "https://github.com/pdong15dth/claudiable_status/releases/download/v#{version}/claudiable_status.zip"
   name "Claudiable Status"
